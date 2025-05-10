cfb4253e5810f76e9202e56c3ca57380f68a7eb4
Glenisha Dsouza <dsouzaglenisha5@gmail.com>
2025-05-10 16:33:07 +0530

    finalcommit

--- q1/t1/src/index.js
+++ q1/t1/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
-impotr ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {ThemeProvider,createTheme} from 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import StockPage from './Pages/stockpage';
+import CorrelationHeatmapPage from './pages/CorrelationHeatmapPage';
+import { Container } from '@mui/material';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Container sx={{ marginTop: 4 }}>
+        <Routes>
+          <Route path="/" element={<StockPage />} />
+          <Route path="/stock" element={<StockPage />} />
+          <Route path="/correlation" element={<CorrelationHeatmapPage />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
